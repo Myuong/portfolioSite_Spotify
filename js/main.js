789e08149984fd008c9cdcaf538d07fa2aa90182
unknown <hjmyung7@gmail.com>
2022-02-17 11:09:08 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+//section3 hover 애니메이션
+$(function(){
+    $(".bill_container").on("mouseenter",function(){
+        $(this).animate({bottom:20},200);
+    }) 
+    
+    .on("mouseleave",function(){
+        $(this).stop().animate({bottom:0},200);
+    });
+
+});
