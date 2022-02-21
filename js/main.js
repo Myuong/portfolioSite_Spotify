46b56958a7ce690e88c830815232396617c73365
Myoung <99623915+Myuong@users.noreply.github.com>
2022-02-21 13:42:34 +0900

    click 수정

--- js/main.js
+++ js/main.js
@@ -2,15 +2,24 @@
 //section3 hover 애니메이션
 $(function(){
     $(".bill_container").on("mouseenter",function(){
-        $(this).animate({bottom:20},200);
+        $(this).stop().animate({bottom:20},200);
     }) 
-    
     .on("mouseleave",function(){
         $(this).stop().animate({bottom:0},200);
-    });
-
+    }); 
 });
 
-/*new WOW().init();*/
+$('.bill_container').click(function(){
+    if($(this).hasClass('on')){
+        $(this).removeClass('on');
+        $(this).stop().animate({bottom:20},200);
+    } else {
+        $('.bill_container').removeClass('on');
+        $(this).stop().animate({bottom:0},200);
+        $(this).addClass('on');
+    }
+});
+
+
 
 AOS.init();
